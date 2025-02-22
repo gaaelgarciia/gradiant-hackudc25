59724efbd1d3efdb6c45011aba7d78d82fc863fd
PepeRoConde <j.rconde@udc.es>
2025-02-22 12:13:58 +0100

    backend: integracion repositorios suma

--- backend/sparql_querys.py
+++ backend/sparql_querys.py
@@ -5,11 +5,17 @@ EX = Namespace("http://127.0.0.1:8000/")
 def consultar_competencia(graph, competencia):
     query = f"""
         PREFIX ex: <{EX}>
-        SELECT DISTINCT ?name ?level WHERE {{
+        SELECT DISTINCT ?name (sum(xsd:integer(coalesce(?level, "0"^^xsd:integer)) + xsd:integer(coalesce(?valor, "0"^^xsd:integer))) AS ?suma) 
+        WHERE {{
             ?person a ex:Person ;
                    ex:name ?name ;
                    ?rel "{competencia}" .
             ?rel owl:hasValue ?level .
+            optional {{ ?person a ex:Person ;
+                            ex:repositories ?repo .
+                        ?repo ex:lenguaje "{competencia}" . 
+                        ex:lenguaje owl:hasValue ?valor }}.
+            
         }}
         ORDER BY DESC(xsd:integer(?level))
     """
@@ -17,7 +23,7 @@ def consultar_competencia(graph, competencia):
     results = []
     for row in graph.query(query):
         name = str(row.name)
-        level = str(row.level).replace('.', '')  # Remove decimal point
+        level = str(row.suma).replace('.', '')  # Remove decimal point
         results.append([name, level])
     return results
 
