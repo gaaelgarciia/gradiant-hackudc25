2a9da16ae862cbb2799de6e0badb89b45f3d79f3
PepeRoConde <j.rconde@udc.es>
2025-02-22 03:41:50 +0100

    vuelve a ir miau

--- backend/sparql_querys.py
+++ backend/sparql_querys.py
@@ -12,24 +12,45 @@
 #        }}
 #    """
 #    return [str(row[0]).split("/")[-1] for row in graph.query(query)]
+
+
+
+
+
+
+#from rdflib import Namespace
+#
+#EX = Namespace("http://127.0.0.1:8000/")
+#
+#def consultar_competencia(graph, competencia):
+#    query = f"""
+#        PREFIX ex: <{EX}>
+#        SELECT DISTINCT ?persona WHERE {{
+#            ?persona rdf:type ex:Person ;
+#                ?nivel "{competencia}" .
+#        }}
+#    """
+#    #return [str(row[0]).split("/")[-1] for row in graph.query(query)]
+#    return graph.query
+#
+#
+#def consultar_personas(graph, competencias):
+#    resultado = []
+#    for competencia in competencias:
+#        resultado.append(consultar_competencia(graph, competencia))
+#    return resultado
+
+
 from rdflib import Namespace
 
 EX = Namespace("http://127.0.0.1:8000/")
 
-def consultar_competencia(graph, competencia):
+def consultar_personas(graph, competencia):
     query = f"""
         PREFIX ex: <{EX}>
         SELECT DISTINCT ?persona WHERE {{
-            ?persona rdf:type ex:Person ;
+            ?persona a ex:Person ;
                 ?nivel "{competencia}" .
         }}
     """
-    #return [str(row[0]).split("/")[-1] for row in graph.query(query)]
-    return graph.query
-
-
-def consultar_personas(graph, competencias):
-    resultado = []
-    for competencia in competencias:
-        resultado.append(consultar_competencia(graph, competencia))
-    return resultado
+    return [str(row[0]).split("/")[-1] for row in graph.query(query)]
